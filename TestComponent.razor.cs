35a694ff9c46e63bc275d7f2b8576b99575ea664
kaleidocore <ola@kaleidocore.com>
2021-02-27 01:25:30 +0100

    Add files via upload

---
+++ TestComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorGenericsBugLib
+{
+		// Any nullable constraints (class, Enum, MyType...) won't compile if the <Nullable> setting is [enable] in Directory.Build.props
+		// It compiles fine if you alter the constraints to 'struct' or set the <Nullable> option to [disable] in the Directory.Build.props
+
+		public partial class TestComponent<T> : TestComponentBase<T>
+				where T : class  // Here... 
+		{
+				public int TestVal { get; set; }
+		}
+
+		public class TestComponentBase<T> : ComponentBase
+				where T : class // And here....
+		{
+				public int BaseVal { get; set; }
+		}
+}
